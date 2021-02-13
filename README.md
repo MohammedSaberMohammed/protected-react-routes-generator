878373a8d0605d57dfc7ccd5cca0d31f12169729
Mohammed Saber <mohammedsaber43@gmail.com>
2021-02-13 20:52:24 +0200

    update README File

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 // Utility
-import RoutesConfiguration from 'protected-react-routes-generators';
+import RoutesConfiguration from 'protected-react-routes-generator';
 // Routes
 import CoursePayment from '../Payment/Course';
 import PaymentResult from '../Payment/PaymentResult';
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom'; // Utils
 
 import { has, isValidComp } from './Utils';
@@ -12,6 +12,7 @@ import { has, isValidComp } from './Utils';
   * @param {object} [structure.authorizedStructure] - it's an object that has [ fallbackPath: {string}, routes: {array} ], fallbackPath: is used for redirect when a logged [in] user tried to access unAuthorized route, routes: only The Logged [in] Routes Available
   * @param {object} [structure.unAuthorizedStructure] - it's an object that has [ fallbackPath: {string}, routes: {array} ], fallbackPath: is used for redirect when a logged [out] user tried to access route that requires [Authorization] , routes: only The Logged [out] Routes Available
   * @param {component} [fallbackComponent] - in order to redirect in all cases if the route doesn't match.
+  * @returns {Array}
 */
 
 export default (structure => {
@@ -20,7 +21,7 @@ export default (structure => {
     anonymousStructure = {},
     authorizedStructure = {},
     unAuthorizedStructure = {},
-    fallbackComponent
+    fallbackComponent = Component
   } = structure || {};
   const dynamicRoutes = [];
 
@@ -51,7 +52,7 @@ export default (structure => {
  * component: React.Component
  * routeProps: Object -----> To override route props
  * redirectPath: String ----> To redirect to specific location
- * condition: to override when to show the component or when to [ Redirect ]
+ * showRouteIf: to override when to show the component or when to [ Redirect ]
  */
 
 const routesGenerator = (isAuthenticated = false, routeSet = {}, type = 'anonymous') => {
@@ -72,34 +73,36 @@ const routesGenerator = (isAuthenticated = false, routeSet = {}, type = 'anonymo
           component,
           routeProps = {},
           redirectPath = '',
-          condition = true
-        } = route || {}; // check the mandatory props for a routes
-        // [=== And ===] Skip [fallbackPath] in case of anonymous
+          showRouteIf = true
+        } = route || {}; // Show Route only [ in The list ] if this prop is true 
 
-        if (!path || (!isAnonymous ? !fallbackPath : false) || !isValidComp(component)) {
-          console.warn(`A [route] is skipped because one of the following, 1/ No valid [path] prop provided for the route, 2/ No valid [component] provided for the route, ${!isAnonymous ? `3/ No [fallbackPath] for ${type} Set` : ''}`);
-        } else {
-          const renderCondition = (isAuthorized ? isAuthenticated : !isAuthenticated) && condition; // In case of Anonymous Routes ===> Just generate without Any Logic
+        if (showRouteIf) {
+          // check the mandatory props for a routes
+          if (!path || !isValidComp(component)) {
+            console.warn(`A [route] is skipped because one of the following, 1/ No valid [path] prop provided for the route, 2/ No valid [component] provided for the route`);
+          } else {
+            const renderCondition = isAuthorized ? isAuthenticated : !isAuthenticated; // In case of Anonymous Routes ===> Just generate without Any Logic
 
-          if (isAnonymous) {
-            return generatedRoutes.push( /*#__PURE__*/React.createElement(Route, _extends({
-              exact: true,
-              key: `${path}_${index}`,
-              path: path,
-              component: () => component
-            }, routeProps)));
-          }
-
-          return generatedRoutes.push( /*#__PURE__*/React.createElement(Route, {
-            exact: true,
-            key: `${route.path}_${index}`,
-            path: route.path,
-            render: props => {
-              return renderCondition ? component : /*#__PURE__*/React.createElement(Redirect, {
-                to: redirectPath || fallbackPath
-              });
+            if (isAnonymous) {
+              return generatedRoutes.push( /*#__PURE__*/React.createElement(Route, _extends({
+                exact: true,
+                key: `${path}_${index}`,
+                path: path,
+                component: () => component
+              }, routeProps)));
             }
-          }));
+
+            return generatedRoutes.push( /*#__PURE__*/React.createElement(Route, {
+              exact: true,
+              key: `${route.path}_${index}`,
+              path: route.path,
+              render: props => {
+                return renderCondition ? component : /*#__PURE__*/React.createElement(Redirect, {
+                  to: redirectPath || fallbackPath
+                });
+              }
+            }));
+          }
         }
       });
     }
